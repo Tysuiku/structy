c3350ffde2e6393f97b5245d65bc12a84170f014
Tysuiku <timothydong41@gmail.com>
2023-08-22 21:57:45 -0400

    added comments and practiced

--- structy.py
+++ structy.py
@@ -16,3 +16,19 @@ def is_prime(n):
     if n % i == 0:
       return False
   return True
+
+def uncompress(s):
+  numbers = "0123456789"
+  result = ""
+  i = 0
+  j = 0
+  while j < len(s):
+    if s[j] in numbers:
+      j+=1
+    else: 
+      num = int(s[i:j])
+      result += s[j] * num
+      j += 1
+      i = j
+    
+  return result
